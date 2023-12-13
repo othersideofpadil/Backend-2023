681e54991e8227bd85ef9ef3d17bad9734b4ac87
Muhammad Fadhillah <114470786+padilkecebanget@users.noreply.github.com>
2023-12-13 07:22:17 +0700

    Delete task9 directory

--- task9/data/fruits.js
+++
@@ -1,9 +0,0 @@
-/**
- * TODO 1:
- * - Buat array yang berisi data buah.
- * - Refactor variable ke ES6 Variable.
- */
-const fruits = ["Anggur" , "Mangga" , "Durian", "Buah Naga", "Rambutan"];
-
-// TODO 2: export variable fruits
-module.exports = fruits;
